5b22345570338fd967faa74e35354367811af463
Matus Minarovic <matusm44@gmail.com>
2018-11-03 13:08:25 +0000

    Canvas initialized

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-`use strict`
+'use strict';
 
-const GAME_WIDTH = 1280;
-const GAME_HEIGHT = 720;
 
-const canvas = document.getElementById("canvas")
+const GAME_HEIGHT = window.innerHeight;
+const GAME_WIDTH = GAME_HEIGHT * (16.0 / 9.0); // adjust ratio
+
+const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-context.beginPath();
-context.moveTo(0, 0);
-context.lineTo(1280, 720);
-context.stroke();
+
+
+function init(){
+    canvas.width = GAME_WIDTH;
+    canvas.height = GAME_HEIGHT;
+
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(GAME_WIDTH, GAME_HEIGHT);
+    context.stroke();
+}
+
+init();
