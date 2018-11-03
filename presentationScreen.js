527c710002db2cf8bb33682d0e5789b00fdd7b85
Hopson97 <mhopson@hotmail.co.uk>
2018-11-03 14:02:19 +0000

    make more screen things

---
+++ presentationScreen.js
@@ -0,0 +1,13 @@
+class PresentationScreen {
+    constructor() {
+        
+    }
+
+    update() {
+
+    }
+
+    redraw() {
+        
+    }
+}
---
+++ registrationScreen.js
@@ -0,0 +1,13 @@
+class RegistrationScreen {
+    constructor() {
+        
+    }
+
+    update() {
+
+    }
+
+    redraw() {
+        
+    }
+}
