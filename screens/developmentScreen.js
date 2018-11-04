ce556d89059b5789b0963ffd54518ecd28f2c127
Matus Minarovic <matusm44@gmail.com>
2018-11-04 15:47:29 +0000

    Winning and losing conditions and alert

--- screens/developmentScreen.js
+++ screens/developmentScreen.js
@@ -66,7 +66,14 @@ class DevelopementScreen {
     }
 
     update() {
-        this.map.collisions(this.player); 
+        if (youLost()){
+            alert("You lost");
+            switchScreen(MAIN_MENU_SCREEN);
+        } else if (youWon()){
+            alert("You won");
+            switchScreen(MAIN_MENU_SCREEN);
+        }
+        this.map.collisions(this.player);
         this.player.update();
         this.mob.update();
         this.mob2.update();
